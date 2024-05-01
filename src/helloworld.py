afb77929894f473ffeeab1385c3b21f8a32711ae
miloertola <milo@redbabel.com>
2024-05-01 15:00:17 +0200

    fix

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,7 +1,14 @@
+from transformers import pipeline
+# import gradio as gr
 
 
-def say_hello():
-	print("Hello World")
- 
- 
-say_hello()
+model = pipeline("text-generation")
+
+def predict(prompt):
+    completion = model(prompt)[0]["generated_text"]
+    return completion
+
+prediction = predict("My favorite programming language is")
+# demo = gr.Interface(fn=predict, inputs="text", outputs="text").launch(share=True)
+
+print(prediction)
