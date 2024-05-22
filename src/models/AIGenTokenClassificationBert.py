d88bfb4d8995252e478c5c7b00e2362a81b498e6
miloertola <milo@redbabel.com>
2024-05-22 16:33:21 +0200

    fix tests

--- src/models/AIGenTokenClassificationBert.py
+++ src/models/AIGenTokenClassificationBert.py
@@ -1,5 +1,6 @@
-from typing import Tuple, List, Enum
+from typing import Tuple, List
 import asyncio
+from enum import Enum
 from concurrent.futures import ThreadPoolExecutor
 from transformers import pipeline, Pipeline, PreTrainedModel, AutoTokenizer
 from src.models.AIGen import AIGen
@@ -10,9 +11,10 @@ class TOKEN_TYPE(Enum):
   ADJ = "ADJ"
 
 
-class AIGenTokenClassification(AIGen):
-  def __init__(self, model_id: str):
+class AIGenTokenClassificationBert(AIGen):
+  def __init__(self, model_id: str, aggregation_strategy: str = "simple"):
     super().__init__(model_id=model_id)
+    self.aggregation_strategy = aggregation_strategy
 
   def ai_init(
     self,
@@ -23,7 +25,7 @@ class AIGenTokenClassification(AIGen):
     self.logger.debug("start create pipeline")
     self.pipeline: Pipeline = pipeline(
       model=self.model_id,
-      aggregation_strategy="simple",
+      aggregation_strategy=self.aggregation_strategy,
     )
     self.logger.debug("end create pipeline")
     self.model = self.pipeline.model
@@ -31,6 +33,10 @@ class AIGenTokenClassification(AIGen):
     return self.model, self.processor
 
   async def generate_token_list(self, text: str, token_type: TOKEN_TYPE) -> List[str]:
+    if text is None or len(text) == 0:
+      raise ValueError("text is null or empty")
+    if token_type is None:
+      raise ValueError("token type is null")
     loop = asyncio.get_running_loop()
     pip: Pipeline = self.pipeline
     self.logger.debug(f"start tokenization {token_type.value}")
