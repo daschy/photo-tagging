3d09f98bd71610177b5b08b9158609ed388d0092
miloertola <milo@redbabel.com>
2024-05-22 16:59:59 +0200

    added reverse geotagging class

--- src/models/AIGen.py
+++ src/models/AIGen.py
@@ -13,11 +13,11 @@ ProcessorT = TypeVar("ProcessorT", bound=ProcessorMixin | AutoTokenizer)
 
 class AIGen(Base, Generic[ModelT, ProcessorT]):
   def __init__(self, model_id: str):
+    super().__init__()
     self.model_id = model_id
     self.model: ModelT = None
     self.processor: ProcessorT = None
     self.pipeline: Pipeline = None
-    self.logger = get_logger(__name__)
     self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
   @abstractmethod
--- src/models/Base.py
+++ src/models/Base.py
@@ -1,5 +1,11 @@
+from logging import Logger
+
+from src.utils.logger_utils import get_logger
+
+
 class Base:
-  pass
+  def __init__(self):
+    self.logger = get_logger(__name__)
 
   def __str__(self):
     attributes = [f"{key}: {value}" for key, value in self.__dict__.items()]
