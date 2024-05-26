ebc3ea526e78c8ef7d638930b9b6c0fdce56a642
miloertola <milo@redbabel.com>
2024-05-26 13:49:47 +0200

    fix

--- src/utils/db_utils_async.py
+++ src/utils/db_utils_async.py
@@ -5,19 +5,19 @@ from models.orm.BaseOrm import BaseOrm
 
 
 def get_db_session(engine) -> sessionmaker:
-  db_session = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-  )
-  return db_session
+	db_session = sessionmaker(
+		bind=engine,
+		class_=AsyncSession,
+		expire_on_commit=False,
+	)
+	return db_session
 
 
 async def init_engine(db_connection_string: str) -> AsyncEngine:
-  try:
-    engine = create_async_engine(db_connection_string, echo=False)
-    async with engine.begin() as conn:
-      await conn.run_sync(BaseOrm.metadata.create_all)
-    return engine
-  except Exception as _:
-    raise
+	try:
+		engine = create_async_engine(db_connection_string, echo=False)
+		async with engine.begin() as conn:
+			await conn.run_sync(BaseOrm.metadata.create_all)
+		return engine
+	except Exception as _:
+		raise
