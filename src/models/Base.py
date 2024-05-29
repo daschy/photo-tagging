e7badc1f60c92500e6c237f120d0db0df04ca682
miloertola <milo@redbabel.com>
2024-05-29 11:51:34 +0200

    file formatting

--- src/models/Base.py
+++ src/models/Base.py
@@ -5,13 +5,13 @@ from utils.logger_utils import get_logger
 
 
 class Base(ABC):
-  def __init__(self):
-    class_name: str = f"{type(self)}"
-    self.logger: Logger = get_logger(f"{class_name.split('.')[-1]}")
+	def __init__(self):
+		class_name: str = f"{type(self)}"
+		self.logger: Logger = get_logger(f"{class_name.split('.')[-1]}")
 
-  def __str__(self):
-    attributes = [f"{key}: {value}" for key, value in self.__dict__.items()]
-    return ", ".join(attributes)
+	def __str__(self):
+		attributes = [f"{key}: {value}" for key, value in self.__dict__.items()]
+		return ", ".join(attributes)
 
-  def __repr__(self):
-    return self.__str__()
+	def __repr__(self):
+		return self.__str__()
